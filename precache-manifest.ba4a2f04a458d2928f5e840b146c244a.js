70d3fd97c93b5ecf3291bdd1ab84cb73982a8094
Irina B <bl00dyhll@yahoo.com>
2020-10-15 11:08:46 +0300

    Updates

---
+++ precache-manifest.ba4a2f04a458d2928f5e840b146c244a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98db0b3f7db30096d7a29847dc8ae433",
+    "url": "/planner-app/index.html"
+  },
+  {
+    "revision": "f9712e53805dccfd37af",
+    "url": "/planner-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "5e3cf852fb8289642119",
+    "url": "/planner-app/static/js/2.60363ccf.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/planner-app/static/js/2.60363ccf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f9712e53805dccfd37af",
+    "url": "/planner-app/static/js/main.477aa263.chunk.js"
+  },
+  {
+    "revision": "f1b57e761c74680d3f8c",
+    "url": "/planner-app/static/js/runtime-main.ac428840.js"
+  }
+]);
