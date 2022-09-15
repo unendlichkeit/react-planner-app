c576ae83e21b356122e0fc018d4390eefb3ba767
Burcea <irina.burcea@hp.com>
2022-09-15 14:02:02 +0300

    ceva header comp modificat laptop hp

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -2,15 +2,17 @@ import React from 'react';
 import {Link} from 'react-router-dom';
 import {auth} from '../../firebase/firebaseInit';
 import {connect} from 'react-redux';
+import './Header.scss';
 
 const Header = ({currentUser}) => {
     //if logged in, add Tasks link
     return (
         <div>
+            <Link className='menuLink' to='/'>HOME</Link>
             {
                 currentUser ?
                 <div><div onClick={() => auth.signOut()}>SIGN OUT</div><span>{currentUser.email}</span></div> :
-                <Link className='signin' to='/signIn'>SIGN IN</Link>
+                <Link className='menuLink signin' to='/signIn'>SIGN IN</Link>
             }
         </div>
     )  
